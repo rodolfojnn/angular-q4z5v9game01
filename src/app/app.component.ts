525c4c7f25f4b402a416f55af2fc4adc947f86fa
rodolfojnn <rodolfojnn@hotmail.com>
2019-11-15 13:05:13 -0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
+  template: '<div id="gameContainer"></div>',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
