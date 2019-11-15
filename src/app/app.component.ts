f34124b4b760ff7ed943e5d8a844b24d022ef1c7
rodolfojnn <rodolfojnn@hotmail.com>
2019-11-15 13:06:10 -0300

    2 files modified, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
+import { NewScene } from './scenes/new-scene';
+import Phaser from 'phaser';
+
 
 @Component({
   selector: 'my-app',
-  template: '<div id="gameContainer"></div>',
-  styleUrls: [ './app.component.css' ]
+  template: '<div id="gameContainer"></div>'
 })
 export class AppComponent  {
-  name = 'Angular';
+  game: Phaser.Game;
+  config: Phaser.Types.Core.GameConfig;
+
+  constructor(public newScene: NewScene) {}
+
+  ngOnInit() {
+    this.config = {      
+      title: 'Game Title',
+      version: '1.0',
+      type: Phaser.AUTO,
+      width: 800, // window.innerWidth,
+      height: 600, // window.innerHeight,
+      scene: [this.newScene],
+      physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+      }
+    }
+    this.game = new Phaser.Game(this.config);   
+       
+  }
 }
