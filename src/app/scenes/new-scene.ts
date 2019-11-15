9e28a0bff973bd7edb8558ab7982e8f564325195
rodolfojnn <rodolfojnn@hotmail.com>
2019-11-15 13:21:57 -0300

    1 file modified

--- src/app/scenes/new-scene.ts
+++ src/app/scenes/new-scene.ts
@@ -20,7 +20,7 @@ export class NewScene extends Phaser.Scene {
   }
 
 public preload() {    
-    const baseAssets = "./assets/"; 
+    const baseAssets = "https://raw.githubusercontent.com/photonstorm/phaser3-examples/master/public/src/games/firstgame/assets/";
     this.load.image('sky', baseAssets + 'sky.png');
     this.load.image('ground', baseAssets + 'platform.png');
     this.load.image('star', baseAssets + 'star.png');
